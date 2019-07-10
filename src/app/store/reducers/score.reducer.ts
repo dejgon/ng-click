5ab36808145bba1c5c0fd599b5878121c04b7d28
Damian Cop <dcop@EC>
2019-07-10 08:16:44 +0200

    .

---
+++ src/app/store/reducers/score.reducer.ts
@@ -0,0 +1,9 @@
+import * as ScoreActions from '../actions/score.actions';
+
+const initialState = 0;
+
+export function scoreReducer(state: Score = getScore(), action: ScoreActions.Actions) {
+    switch(action.type) {
+        
+    }
+}
